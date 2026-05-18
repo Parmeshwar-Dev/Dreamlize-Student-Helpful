ba6ae9b81cfc614f07535b1db70bfc9b6d53af5e
Parmeshwar <devparm06@gmail.com>
2026-05-18 22:35:12 +0530

    Changing the seocnds in Splash Activity

--- app/src/main/java/com/mountreach/paracticeapp/SplashScreenActivity.java
+++ app/src/main/java/com/mountreach/paracticeapp/SplashScreenActivity.java
@@ -48,7 +48,7 @@ public class SplashScreenActivity extends AppCompatActivity {
                            startActivity(intent);
                            finish();
                        }
-                   },1000);
+                   },2000);
                }
 
            }
