1f35393a776aa1dff0e675d2526e966b726bb8d4
Parmeshwar <devparm06@gmail.com>
2026-05-18 22:30:03 +0530

    Initial project upload

---
+++ app/src/main/java/com/mountreach/paracticeapp/SplashScreenActivity.java
@@ -0,0 +1,59 @@
+package com.mountreach.paracticeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    TextView tvsplash;
+
+    String appName = "DocLink";
+    int index = 0;
+    long dealy = 200;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        tvsplash = findViewById(R.id.tvsplash);
+
+     Handler handler = new Handler();
+       handler.postDelayed(new Runnable() {
+           @Override
+           public void run() {
+
+               if(index<= appName.length())
+               {
+                    tvsplash.setText(appName.substring(0,index));
+                    index++;
+                    handler.postDelayed(this,dealy);
+               }
+               else
+               {
+                   new Handler().postDelayed(new Runnable() {
+                       @Override
+                       public void run() {
+                           Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                           startActivity(intent);
+                           finish();
+                       }
+                   },1000);
+               }
+
+           }
+       },dealy);
+
+
+    }
+}
